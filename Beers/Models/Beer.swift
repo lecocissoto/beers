7ecbad4a5f7836d75f2b7c9877b78a75a718e361
Leandro Cissoto <lecocissoto@gmail.com>
2017-07-18 23:56:19 -0300

    Add CollectionViewController and embed navigation controller

---
+++ Beers/Models/Beer.swift
@@ -0,0 +1,9 @@
+//
+//  Beer.swift
+//  Beers
+//
+//  Created by Leandro Cissoto Ramos on 7/18/17.
+//  Copyright © 2017 Cissoto Ramos Tecnologia da Informacao. All rights reserved.
+//
+
+import Foundation
