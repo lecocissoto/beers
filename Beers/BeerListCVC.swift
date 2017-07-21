e50b21a2fd98bee37325e5094e385e58a8285629
Leandro Cissoto <lecocissoto@gmail.com>
2017-07-21 09:38:49 -0300

    Add cell size to fit two beers :beers:

--- Beers/BeerListCVC.swift
+++ Beers/BeerListCVC.swift
@@ -12,28 +12,49 @@ private let reuseIdentifier = String(describing: BeerCVCell.self)
 
 class BeerListCVC: UICollectionViewController {
     
+    @IBOutlet var spinnerView: SpinnerView!
+    
+    // Properties
+    var isLoading      = false {
+        didSet{
+            self.toggleSpinner(isVisible: self.isLoading)
+        }
+    }
+    
     var beers: [Beer]  = [] {
         didSet{ self.collectionView?.reloadData() }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        API(env: .production, version: .v2).beerService.beers(success: { (msg, beers) in
-            self.beers = beers
-            
-        }) { (error, message) in
-            let alert = UIAlertController(title: "Ops!", message: message, preferredStyle: .alert)
-            let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-            alert.addAction(action)
-            
-            self.present(alert, animated: true, completion: nil)
-        }
+        requestForBeers()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    private func requestForBeers(){
+        isLoading = true
+        API(env: .production, version: .v2).beerService.beers(success: { (msg, beers) in
+            self.beers = beers
+            self.isLoading = false
+            
+        }) { (error, message) in
+            self.isLoading = false
+            
+            let alert        = UIAlertController(title: "Ops!", message: message, preferredStyle: .alert)
+            let action       = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            let refresAction = UIAlertAction(title: "Refresh", style: .default, handler: { (action) in
+                self.requestForBeers()
+            })
+            
+            alert.addAction(action)
+            alert.addAction(refresAction)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
     // MARK: - Navigation
 
@@ -46,26 +67,69 @@ class BeerListCVC: UICollectionViewController {
             nextVC.beer = beers[indexPath.row]
         }
     }
- 
+    
+    // MARK: Spinner Helper
+    // TODO: Extract this to a ViewControllerExtension ;)
+    func displaySpinner(){
+        spinnerView.layer.opacity = 0.0
+        view.addSubview(spinnerView)
+        spinnerView.center = view.center
+                                    
+        UIView.animate(withDuration: 0.20) {
+            self.spinnerView.layer.opacity = 0.5
+        }
+    }
+    
+    func hideSpinner(){
+        UIView.animate(withDuration: 0.20, animations: {
+            self.spinnerView.layer.opacity = 1
+        }) { (completion) in
+            self.spinnerView.removeFromSuperview()
+        }
+    }
+    
+    func toggleSpinner(isVisible: Bool){
+        (isVisible) ? displaySpinner() : hideSpinner()
+    }
+}
 
-    // MARK: UICollectionViewDataSource
+// MARK: UICollectionViewDataSource
 
+extension BeerListCVC {
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return beers.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! BeerCVCell
         cell.configureCell(with: beers[indexPath.row])
-    
+        
         return cell
     }
-    
 }
+
+//MARK: Scrollview Flow Layout
+extension BeerListCVC: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let cellDivider: CGFloat =  2.0
+        
+        let size = CGSize(width: self.view.frame.width / 2 - 11, height: view.frame.size.height / cellDivider + 5)
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+    }
+}
+
+
